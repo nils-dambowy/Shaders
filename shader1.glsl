e1ef4a82a0baa9f618f3ee08d4db24909d300a1c
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-02-09 09:03:46 +0100

    played around with test.glsl and made my own version

---
+++ shader1.glsl
@@ -0,0 +1,23 @@
+// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+// Created by S.Guillitte
+void main() {
+    float time = iGlobalTime * 1.0;
+    vec2 uv = (gl_FragCoord.xy / iResolution.xx - 0.5) * 8.0;
+    
+    float i0 = 3.0;
+    float i1 = 1.25;
+    float i2 = 1.0;
+    float i4 = 0.0;
+    for(int i = 0; i < 6; i++){
+       vec2 r;
+       r = vec2(cos(uv.y * i0 - i4 + time / i1), sin(uv.x * i0 - i4 + time / i1)) / i2;
+       r += vec2(-r.y, r.x) * 0.2;
+       uv.xy = r;
+    }
+    float r = sin(uv.x - time) * 0.5 + 0.5;
+    float b = sin(uv.y + time) * 0.5 + 0.5;
+    float g = sin((uv.x + uv.y + sin(time * 0.5)) * 0.5) * 0.5 + 0.5;
+    
+    gl_FragColor = vec4(r, g, b, 1.0);
+    
+}
